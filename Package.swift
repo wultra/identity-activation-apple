ed3b0d4337acc8032138e87d01c76b47a4e876dd
Jan Kobersky <kober32@gmail.com>
2023-05-18 11:53:56 +0200

    Added SPM support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "WultraDigitalOnboarding",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "WultraDigitalOnboarding", targets: ["WultraDigitalOnboarding"])
+    ],
+    dependencies: [
+        .package(name: "PowerAuth2", url: "https://github.com/wultra/powerauth-mobile-sdk-spm.git", .upToNextMinor(from: "1.7.8")),
+        .package(name: "WultraPowerAuthNetworking", url: "https://github.com/wultra/networking-apple.git", .upToNextMinor(from: "1.1.8"))
+    ],
+    targets: [
+        .target(
+            name: "WultraMobileTokenSDK",
+            dependencies: ["PowerAuth2", .product(name: "PowerAuthCore", package: "PowerAuth2"), "WultraPowerAuthNetworking"],
+            path: "Sources"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
